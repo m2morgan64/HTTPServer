ba6bf3a4681b59dae6ce93fed0fa713b8e48b56d
Matt Daddy <m2morgan64@gmail.com>
2018-03-07 17:19:20 -0500

    Initial Stubs

---
+++ TCPServer/lib/EventHelpers.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPServer.lib
+{
+    public static class EventHelpers
+    {
+        public static void RaiseSingleStringEvent(object obj, EventHandler<SingleStringEventArgs> evt, string val)
+        {
+            if (evt != null)
+            {
+                evt(obj, new SingleStringEventArgs(val));
+            }
+        }
+
+        public static void RaiseMultiStringEvent(object obj, EventHandler<MultiStringEventArgs> evt, List<string> vals)
+        {
+            if (evt != null)
+            {
+                evt(obj, new MultiStringEventArgs(vals));
+            }
+        }
+
+        public static void RaiseMultiStringEvent(object obj, EventHandler<MultiStringEventArgs> evt, params string[] vals)
+        {
+            if (evt != null)
+            {
+                evt(obj, new MultiStringEventArgs(vals));
+            }
+        }
+
+        public class SingleStringEventArgs : System.EventArgs
+        {
+            public string Value { get; protected set; }
+            public SingleStringEventArgs(string val)
+            {
+                Value = val;
+            }
+        }
+
+        public class MultiStringEventArgs : System.EventArgs
+        {
+            public List<string> Values { get; protected set; }
+            public MultiStringEventArgs(List<string> vals)
+            {
+                Values = vals;
+            }
+
+            public MultiStringEventArgs(params string[] vals)
+            {
+                Values.AddRange(vals);
+            }
+        }
+    }
+}
---
+++ TCPServer/lib/HTTPPrefixes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static TCPServer.lib.EventHelpers;
+
+namespace TCPServer.lib
+{
+    public class HTTPPrefixes<T> : List<T>
+    {
+        public event EventHandler<SingleStringEventArgs> OnAdd;
+        public event EventHandler<MultiStringEventArgs> OnAddRange;
+        public event EventHandler<SingleStringEventArgs> OnRemove;
+        public event EventHandler<MultiStringEventArgs> OnRemoveRange;
+        
+        public new void Add(T item)
+        {
+            RaiseSingleStringEvent(this, OnAdd, item as string);
+            base.Add(item);
+        }
+
+        public new void AddRange(IEnumerable<T> items)
+        {
+            RaiseMultiStringEvent(this, OnAddRange, items as List<string>);
+            base.AddRange(items);
+        }
+
+        public new void Remove(T item)
+        {
+            RaiseSingleStringEvent(this, OnRemove, item as string);
+            base.Remove(item);
+        }
+
+        public new void RemoveRange(Int32 index, Int32 count)
+        {
+            RaiseSingleStringEvent(this, OnRemove, item as string);
+            base.RemoveRange(index, count);
+        }
+    }
+}
